c1031905f505b319762db265a11a8eb3a1aa3c9e
adejokeajibade <adejokeajibade01@gmail.com>
2021-08-11 23:22:17 +0100

    Update main.php

--- Calculator/src/main.php
+++ Calculator/src/main.php
@@ -4,22 +4,16 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 use Calculator\StringCalculator;
 
- // $numbers = $_POST['numbers'];
-   //print_r ($numbers);
-  //$numbers = "//;;;\n1;;;3;;;4;;;2000";
-  $numbers = "//@\n1@3@4";
+  $numbers = $_POST['numbers'];
   $calculate_string = new StringCalculator();
   $result =  $calculate_string->add($numbers);
   
-  echo $numbers;
-  echo $result
-  
     //Pass values into session
-	/* session_start();
+	 session_start();
 	$_SESSION['result'] = $result;
 	
 	header('Location: calculate.php');
-	exit(); */ 
+	exit(); 
   
 
 
